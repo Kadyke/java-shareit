18b2673504ae1c26e8902252d574da0a991547fa
Cezare_Borgia <yndx-petrov-dmit@yandex.ru>
2023-06-18 17:41:16 +0400

    style

--- src/main/java/ru/practicum/shareit/item/Item.java
+++ src/main/java/ru/practicum/shareit/item/Item.java
@@ -17,8 +17,11 @@ public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "description")
     private String description;
+    @Column(name = "available")
     private Boolean available;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")

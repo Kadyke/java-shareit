a87c6af5b9caa29ca071191d594e4984a45314d2
Cezare_Borgia <yndx-petrov-dmit@yandex.ru>
2023-07-03 21:04:51 +0400

    feat: добавить докер

--- gateway/src/main/java/ru/practicum/shareit/exception/ExceptionController.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ExceptionController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Map;
 
 @RestControllerAdvice("ru.practicum.shareit")
@@ -20,12 +19,6 @@ public class ExceptionController {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public Map<String, String> handleForbiddenAccessException(final ForbiddenAccessException e) {
-        return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public Map<String, String> handleEntityNotFoundException(final EntityNotFoundException e) {
         return Map.of("error", e.getMessage());
     }
 
